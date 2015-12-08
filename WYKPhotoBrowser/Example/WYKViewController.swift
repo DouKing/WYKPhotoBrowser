975688f3e7183e9fad6f4217f7bb8adfbff66b1f
Wu Yikai <wuyikai@secoo.com>
2015-12-08 21:18:46 +0800

    MOD: git initial

---
+++ WYKPhotoBrowser/Example/WYKViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WYKPhotoBrowser
+//
+//  Created by WuYikai on 15/12/8.
+//  Copyright © 2015年 secoo. All rights reserved.
+//
+
+import UIKit
+
+class WYKViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
+
+}
+
